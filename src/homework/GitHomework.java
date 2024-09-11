f197a9ce80c0bf5925cc82343952dc86a2475ce2
cbatres522 <cgusbat2022@gmail.com>
2024-09-10 21:06:43 -0500

    cleared methods

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -1,18 +1,6 @@
 package homework;
 
 public class GitHomework {
-	
-	public void display() {
-		System.out.println("John Cena");
-	}
-	/**
-		This is coming from GitHub
-		Java is fun
-		Git is EASY
-		API is not fun...
-	*/
-
-	
 
 	
 }
