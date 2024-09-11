b9df50cccb7763e266afee9036f35a0f67969ee1
Lbat23 <leebatres0148@gmail.com>
2024-09-10 20:53:23 -0500

    Update GitHomework.java

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -5,6 +5,13 @@ public class GitHomework {
 	public void display() {
 		System.out.println("John Cena");
 	}
+	/**
+		This is coming from GitHub
+		Java is fun
+		Git is EASY
+		API is not fun...
+	*/
+
 	
 
 	
