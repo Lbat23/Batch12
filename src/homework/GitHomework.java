0f0a397324afcbacf20a11b47374e4734e1a1716
cbatres522 <cgusbat2022@gmail.com>
2024-09-10 20:15:54 -0500

    clearing

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -2,26 +2,4 @@ package homework;
 
 public class GitHomework {
 	
-	public static void main (String args []) {
-		System.out.println("Batch 12: Git Class Lecture");
-		int age = 12;
-		String name = "John";
-		//This is coming from github
-		String lastName = "Cena";
-		//This is coming from github
-		String city = "Houston";
-		//This is coming from github
-		
-		
-	}
-	
-	public static void add (int a, int b) {
-		System.out.println(a+b);
-	}
-	public static void add (int a, int b, int c) {
-		System.out.println(a+b+c);
-	}
-	public static void add (int a, int b, int c, int d) {
-		System.out.println(a+b+c+d);
-	}
 }
