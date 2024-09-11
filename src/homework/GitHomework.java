76846b8e899e0ec27443791d5289daf38b80d498
cbatres522 <cgusbat2022@gmail.com>
2024-09-10 19:56:03 -0500

    added methods

--- src/homework/GitHomework.java
+++ src/homework/GitHomework.java
@@ -13,5 +13,10 @@ public class GitHomework {
 	public static void add (int a, int b) {
 		System.out.println(a+b);
 	}
-
+	public static void add (int a, int b, int c) {
+		System.out.println(a+b+c);
+	}
+	public static void add (int a, int b, int c, int d) {
+		System.out.println(a+b+c+d);
+	}
 }
